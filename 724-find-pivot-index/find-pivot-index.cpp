97ab9bf5bdcbed5f136d50dbe6e56ae1ca97ec35
Rohan <rohangupta112005@gmail.com>
2025-01-30 20:03:29 +0530

    Time: 0 ms (100.00%) | Memory: 35.6 MB (77.15%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int totalsum=0,leftsum=0;
+        for(int num : nums) totalsum+=num;
+        for(int i=0;i<nums.size();i++) {
+            if(leftsum == totalsum-leftsum-nums[i]) return i;
+            leftsum+=nums[i];
+        }
+        return -1;
+    }
+};
