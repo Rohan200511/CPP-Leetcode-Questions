a290ec6f01be75d942e98aabb2cef32bcdfb63cb
Rohan <rohangupta112005@gmail.com>
2025-04-06 16:38:26 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (55.37%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string result;
+        int depth=0;
+        for(int ch : s){
+            if(ch=='('){
+                if(depth > 0) result+=ch;
+                depth++;
+            }
+            else{
+                depth--;
+                if(depth>0) result+=ch;
+            }
+        }
+        return result;
+    }
+};
