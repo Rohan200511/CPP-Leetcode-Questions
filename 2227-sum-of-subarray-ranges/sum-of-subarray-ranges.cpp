9816a5a28e775b015cb5531706fef5d206530c0c
Rohan <rohangupta112005@gmail.com>
2025-05-20 19:49:50 +0530

    Time: 14 ms (27.58%) | Memory: 14 MB (75.75%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -6,8 +6,8 @@ public:
         for(int i=0;i<n;i++){
             long long maxi = nums[i];
             long long mini = nums[i];
-            for(int j=i;j<n;j++){
-                maxi = max(maxi,(long long)nums[j]);       // take care of type casting here
+            for(int j=i+1;j<n;j++){
+                maxi = max(maxi,(long long)nums[j]);       
                 mini = min(mini,(long long)nums[j]);
                 sum += maxi - mini;
             }
