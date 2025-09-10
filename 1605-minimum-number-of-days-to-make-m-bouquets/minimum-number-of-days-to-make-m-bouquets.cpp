083cb3a0ad27aa3c3b3ba591c1d881819c83f756
Rohan <rohangupta112005@gmail.com>
2025-09-10 22:13:46 +0530

    Time: 16 ms (91.39%) | Memory: 70.4 MB (76.05%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
-    bool possible(vector<int> &a,int day,int m,int k){
-        int count=0,noB=0;
-        for(int i=0;i<a.size();i++){
-            if(a[i]<=day){
-                count++;
-            }
-            else{
-                noB+=count/k;
-                count=0;
-            }
-        }
-        noB+=count/k;
-        return noB>=m;
-    }
-    int minDays(vector<int>& bloomDay, int m, int k) {
-        long long val=m*1LL*k*1LL;
-        int n=bloomDay.size();
-        if(val>n) return -1;
-        int mini=INT_MAX,maxi=INT_MIN;
+    bool possible(vector<int>& a , int day , int m ,int k){
+        int count = 0 ;
+        int noB = 0;
+        int n = a.size();
         for(int i=0;i<n;i++){
-            maxi=max(maxi,bloomDay[i]);
-            mini=min(mini,bloomDay[i]);
+            if(a[i]<=day) count++;
+            else {
+                noB += count / k;
+                count = 0;
+            }
         }
-        int low=mini,high=maxi;
+        noB += count/k;
+        return noB >= m;
+    }
+    
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        int n = bloomDay.size();
+        int maxi = INT_MIN;
+        int mini = INT_MAX;
+        for(int i = 0;i<n;i++){
+            maxi = max(maxi,bloomDay[i]);        
+            mini = min(mini,bloomDay[i]);        
+        }
+        int low = mini , high = maxi;
+        long long val = m * 1LL* k * 1LL;
+        if(val > n) return -1; 
         while(low<=high){
-            int mid=low+(high-low)/2;
-            if(possible(bloomDay,mid,m,k)) high=mid-1;
-            else low=mid+1;
+            int mid = (low+high)/2;
+            if(possible(bloomDay,mid,m,k)) high = mid-1;
+            else low = mid+1;
         }
         return low;
     }
