c8bdad08d5d0ca748e98c2836a9ca759a8969423
Rohan <rohangupta112005@gmail.com>
2025-09-12 16:46:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
@@ -1,60 +1,17 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    int kthElement(vector<int>& a, vector<int>& b, int k) {
+    int kthElement(vector<int> &a, vector<int> &b, int k) {
         // code here
-        vector<int>res;
-        int x=a.size();
-        int y=b.size();
-        int i=0,j=0;
-        while(i<x){
-            res.push_back(a[i]);
-            i++;
+        int i =0 , j=0;
+        vector<int>merged;
+        
+        while(i<a.size() && j<b.size()){
+            if(a[i] <= b[j]) merged.push_back(a[i++]);
+            else merged.push_back(b[j++]);
         }
-        while(j<y){
-            res.push_back(b[j]);
-            j++;
-        }
-        sort(res.begin(),res.end());
-        return res[k-1];
+        while(i<a.size()) merged.push_back(a[i++]);
+        while(j<b.size()) merged.push_back(b[j++]);
+        
+        return merged[k-1];
     }
 };
-
-//{ Driver Code Starts.
-
-// Driver code
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        int n, m, k;
-        cin >> k;
-        cin.ignore();
-        string input;
-        int num;
-        vector<int> a, b;
-
-        getline(cin, input);
-        stringstream s2(input);
-        while (s2 >> num) {
-            a.push_back(num);
-        }
-
-        getline(cin, input);
-        stringstream s3(input);
-        while (s3 >> num) {
-            b.push_back(num);
-        }
-
-        Solution ob;
-        cout << ob.kthElement(a, b, k) << endl << "~\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
