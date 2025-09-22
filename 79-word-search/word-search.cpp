31c1f4b6f748060bf051a5123d56be64b12fde4b
Rohan <rohangupta112005@gmail.com>
2025-09-22 22:28:11 +0530

    Time: 799 ms (31.27%) | Memory: 11.7 MB (34.01%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
 
-    bool recurse(vector<vector<char>> &board, int index,int row,int col, int n, int m, string word){
-        if(word.length()==index) return true;
-        if(row < 0 || col < 0 || row >= n || col >= m) return false;
-        if(board[row][col] != word[index] || board[row][col] == '#') return false;
+    bool isPossible(vector<vector<char>>& board , string word,int row,int col,int index,int n,int m){
+        if(index == word.length()) return true;
+        if(row<0 || col<0 || row>=n || col>=m) return false;
+        if(board[row][col]!=word[index] || board[row][col] == '$') return false;
         char c = board[row][col];
-        board[row][col] = '#';
+        board[row][col] = '$';
 
-        bool bottom = recurse(board , index+1 , row+1,col,n,m,word);
-        bool top  = recurse(board , index+1 , row-1,col,n,m,word);
-        bool left =  recurse(board , index+1 , row,col-1,n,m,word);
-        bool right =  recurse(board , index+1 , row,col+1,n,m,word);
-        
+        bool up = isPossible(board,word,row-1,col,index+1,n,m);
+        bool right = isPossible(board,word,row,col+1,index+1,n,m);
+        bool down = isPossible(board,word,row+1,col,index+1,n,m);
+        bool left = isPossible(board,word,row,col-1,index+1,n,m);
+
         board[row][col] = c;
-        return top || bottom || left || right;
-        
+        return up || right || down || left;
     }
+
     bool exist(vector<vector<char>>& board, string word) {
         int n = board.size();
         int m = board[0].size();
         int index = 0;
-        for(int i = 0;i<n;i++){
-            for(int j = 0; j<m;j++){
+        for(int i = 0;i < n ; i++){
+            for(int j = 0;j<m;j++){
                 if(board[i][j] == word[index]){
-                    if(recurse(board, index,i,j, n , m , word)) return true;
+                    if(isPossible(board,word,i,j,index,n,m)) return true;
                 }
             }
         }
-            return false;
+        return false;
     }
 };
