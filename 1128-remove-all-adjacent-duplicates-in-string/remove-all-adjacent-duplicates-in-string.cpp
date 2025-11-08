1730c395c59f76c9df29ffb921dc95c22ce00d4f
Rohan <rohangupta112005@gmail.com>
2025-11-08 22:33:35 +0530

    Time: 3 ms (80.30%) | Memory: 13.5 MB (86.21%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string res;
+
+        for(char c : s){
+            if(!res.empty() && res.back() == c){
+                res.pop_back();
+            }
+
+            else{
+                res.push_back(c);
+            }
+        }
+        return res;
+    }
+};
