d69f13c1c7ac00d0c79e6a6762702998aa08b75c
Rohan <rohangupta112005@gmail.com>
2025-11-20 19:17:11 +0530

    Time: 2 ms (22.58%) | Memory: 8.6 MB (79.24%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-   void recurse(int k, int n, int start, vector<vector<int>>& ans, vector<int>& curr){
-    if(curr.size()==k &&n==0) {
-        ans.push_back(curr);
-        return;
-    }
-    if(curr.size()>k || n<0) return;
 
-    for(int i=start;i<=9;i++){
-        curr.push_back(i);
-        recurse(k,n-i,i+1,ans,curr);
-        curr.pop_back();
+    
+    void recurse(int k , int n , vector<vector<int>>& ans , vector<int>& curr , int start){
+        if(k == 0 && n == 0){
+            ans.push_back(curr);
+            return;
+        }
+        if(k == 0 || n <= 0) return;
+        for(int i = start ; i <= 9 ; i++){
+            curr.push_back(i);
+            recurse(k - 1 , n - i , ans , curr , i + 1);
+            curr.pop_back();
+        }
     }
-   }
 
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<vector<int>> ans;
-        vector<int> curr;
-        recurse(k,n,1,ans,curr);
+        if(k < 0 || n < 0) return {};
+        vector<vector<int>>ans;
+        vector<int>curr;
+        recurse(k , n , ans , curr , 1);
         return ans;
     }
 };
