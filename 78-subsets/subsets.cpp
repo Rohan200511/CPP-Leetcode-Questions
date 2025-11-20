ac956d3b18f52244ed756d1bd22212c96ddacaa2
Rohan <rohangupta112005@gmail.com>
2025-11-20 14:06:28 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (59.05%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
+
+    void recurse(vector<int>& nums , int n , vector<int>& curr , vector<vector<int>>& ans , int index){
+        if(index == n){
+            ans.push_back(curr);
+            return;
+        }
+
+        curr.push_back(nums[index]);
+        recurse(nums , n , curr , ans , index+1);
+        curr.pop_back();
+        recurse(nums , n , curr , ans , index+1);
+    }
+
     vector<vector<int>> subsets(vector<int>& nums) {
         int n = nums.size();
-        int Nsubsets = (1 << n);
         vector<vector<int>>ans;
-        for(int num = 0 ; num < Nsubsets ; num++){
-            vector<int>temp;
-            for(int i = 0 ; i < n ; i++){
-                if(num & (1 << i)) temp.push_back(nums[i]);
-            }
-            ans.push_back(temp);
-        }
+        vector<int>curr;
+
+        recurse(nums , n , curr, ans ,0);
         return ans;
     }
 };
