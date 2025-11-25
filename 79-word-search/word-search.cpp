f43dd3f1ac4087008ad55b8104de19f1d3ea5cc8
Rohan <rohangupta112005@gmail.com>
2025-11-25 22:40:56 +0530

    Time: 799 ms (30.62%) | Memory: 11.6 MB (44.33%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
 
-    bool isPossible(vector<vector<char>>& board , string word,int row,int col,int index,int n,int m){
+    bool isPossible(vector<vector<char>>& board , string word , int i , int j , int index , int rows , int cols){
         if(index == word.length()) return true;
-        if(row<0 || col<0 || row>=n || col>=m) return false;
-        if(board[row][col]!=word[index] || board[row][col] == '$') return false;
-        char c = board[row][col];
-        board[row][col] = '$';
+        if(i < 0 || j < 0 || i >= rows || j >= cols) return false;
+        if(board[i][j] != word[index] || board[i][j] == '$') return false;
 
-        bool up = isPossible(board,word,row-1,col,index+1,n,m);
-        bool right = isPossible(board,word,row,col+1,index+1,n,m);
-        bool down = isPossible(board,word,row+1,col,index+1,n,m);
-        bool left = isPossible(board,word,row,col-1,index+1,n,m);
+        char c = board[i][j];
+        board[i][j] = '$';
 
-        board[row][col] = c;
-        return up || right || down || left;
+        bool up = isPossible(board , word , i-1 , j ,index+1 , rows , cols);
+        bool down = isPossible(board , word , i+1 , j ,index+1 , rows , cols);
+        bool left = isPossible(board , word , i , j-1 ,index+1 , rows , cols);
+        bool right = isPossible(board , word , i , j+1 ,index+1 , rows , cols);
+
+        board[i][j] = c;
+        return up || down || left || right;
     }
 
     bool exist(vector<vector<char>>& board, string word) {
         int n = board.size();
         int m = board[0].size();
         int index = 0;
-        for(int i = 0;i < n ; i++){
-            for(int j = 0;j<m;j++){
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
                 if(board[i][j] == word[index]){
-                    if(isPossible(board,word,i,j,index,n,m)) return true;
+                    if(isPossible(board , word , i , j , index , n , m)) return true;
                 }
             }
         }
