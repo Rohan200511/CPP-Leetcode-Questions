b52ca2abb6575bf231f0a9fe12495440ab3dd71c
Rohan <rohangupta112005@gmail.com>
2025-12-02 00:05:45 +0530

    Time: 68 ms (46.78%) | Memory: 73.1 MB (40.77%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.cpp
+++ 1558-course-schedule-iv/course-schedule-iv.cpp
@@ -1,38 +1,49 @@
 class Solution {
-public: 
-
-    bool dfs(int src , int dest , vector<bool>& vis , unordered_map<int , vector<int>>& adj){
-        vis[src] = true;
-
-        if(src == dest) return true;
-        
-        for(auto& v : adj[src]){
-            if(!vis[v]){
-                if(dfs(v , dest , vis , adj)) return true;
-            }
-        }
-        return false;
-    }
-
+public:
     vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
-        unordered_map<int , vector<int>>adj;
+        unordered_map<int,vector<int>>adj;
+        vector<int>indegree(numCourses , 0);
 
-        for(auto it : prerequisites){
+        for(auto& it : prerequisites){
             int u = it[0];
             int v = it[1];
-
             adj[u].push_back(v);
+
+            indegree[v]++;
         }
 
-        int Q = queries.size();
-        vector<bool>res(Q);
-        
-        for(int i = 0 ; i < Q ; i++){
-            vector<bool>vis(numCourses , false);
-            int u = queries[i][0];
-            int v = queries[i][1];
+        vector<bool>res(queries.size() , false);
+        queue<int>q;
 
-            res[i] = dfs(u , v , vis , adj);
+        for(int i = 0 ; i < numCourses ; i++){
+            if(indegree[i] == 0) q.push(i);
+        }
+
+        unordered_map<int , unordered_set<int>>mp;
+
+        while(q.size()){
+            int node = q.front();
+            q.pop();
+
+            for(auto& v : adj[node]){
+                mp[v].insert(node);
+
+                for(auto& pre : mp[node]){
+                    mp[v].insert(pre);
+                }
+
+                indegree[v]--;
+                if(indegree[v] == 0) q.push(v);
+            }
+        }
+
+        for(int i = 0 ; i < queries.size() ; i++){
+            int src = queries[i][0];
+            int dest = queries[i][1];
+
+            bool isReachable = mp[dest].contains(src);
+
+            res[i] = isReachable;
         }
         return res;
     }
