35b92a0ee4bf19d2b7fa1ab9caa64f4fc3046497
Rohan <rohangupta112005@gmail.com>
2025-12-20 00:46:55 +0530

    Time: 23 ms (54.57%) | Memory: 70.4 MB (75.99%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
-    bool possible(vector<int>& a , int day , int m ,int k){
-        int count = 0 ;
-        int noB = 0;
-        int n = a.size();
-        for(int i=0;i<n;i++){
-            if(a[i]<=day) count++;
-            else {
-                noB += count / k;
+
+    bool isPossible(vector<int>& bloomDay , int days , int m , int k){
+        int count = 0;
+        int noofB = 0;
+
+        for(int i = 0 ; i < bloomDay.size() ; i++){
+            if(bloomDay[i] <= days) count++;
+            else{
+                noofB += count / k;
                 count = 0;
             }
         }
-        noB += count/k;
-        return noB >= m;
+        noofB += count / k;
+        return noofB >= m;
     }
-    
+
+
     int minDays(vector<int>& bloomDay, int m, int k) {
-        int n = bloomDay.size();
-        int maxi = INT_MIN;
-        int mini = INT_MAX;
-        for(int i = 0;i<n;i++){
-            maxi = max(maxi,bloomDay[i]);        
-            mini = min(mini,bloomDay[i]);        
-        }
-        int low = mini , high = maxi;
-        long long val = m * 1LL* k * 1LL;
-        if(val > n) return -1; 
-        while(low<=high){
-            int mid = (low+high)/2;
-            if(possible(bloomDay,mid,m,k)) high = mid-1;
-            else low = mid+1;
+        int low = *min_element(bloomDay.begin() , bloomDay.end());
+        int high = *max_element(bloomDay.begin() , bloomDay.end());
+        long long reqd = 1LL * m * 1LL * k;
+        if(reqd > bloomDay.size()) return -1;
+
+        while(low <= high){
+            int mid = low + ((high - low) / 2);
+
+            if(isPossible(bloomDay , mid , m , k)) high = mid - 1;
+            else low = mid + 1;
         }
         return low;
     }
