fedb8d66dfa2e4e598ba8848376541ab34a5fbcb
Rohan <rohangupta112005@gmail.com>
2026-01-16 14:44:52 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (70.16%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int t[46];
+    Solution(){
+        memset(t , -1 , sizeof(t));
+    }
+
+    int climbStairs(int n) {
+        if(n < 0) return 0;
+        if(n == 0) return 1;
+
+        if(t[n] != -1) return t[n];
+
+        return t[n] = climbStairs(n - 1) + climbStairs(n - 2);
+    }
+};
