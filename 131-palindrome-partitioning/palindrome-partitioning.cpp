c5b85369b91851cfe6ff386307817b5de88a6c61
Rohan <rohangupta112005@gmail.com>
2026-02-09 19:32:05 +0530

    Time: 32 ms (80.51%) | Memory: 53.4 MB (92.75%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,34 +1,45 @@
 class Solution {
 public:
 
-    bool isPalindrome(string s , int st , int end){
-        while(st < end){
-            if(s[st++] != s[end--]) return false;
-        }
-        return true;
-    }
-
-    void recurse(string s  , int n , int index , vector<string>& curr , vector<vector<string>>& ans){
-        if(index == n){
+    void solve(int i , const string& s , vector<string>& curr , vector<vector<string>>& ans ,vector<vector<bool>>& t){
+        if(i == s.length()){
             ans.push_back(curr);
             return;
         }
 
-
-        for(int i = index ; i < n ; i++){
-            if(isPalindrome(s , index , i)){
-                curr.push_back(s.substr(index , i - index + 1));
-                recurse(s , n , i + 1 , curr , ans);
+        for(int j = i ; j < s.length() ; j++){
+            if(t[i][j] == true){
+                curr.push_back(s.substr(i , j - i + 1));
+                solve(j+1 , s , curr , ans , t);
                 curr.pop_back();
             }
         }
     }
-    
+
     vector<vector<string>> partition(string s) {
-        if(s.empty()) return {};
+        int n = s.length();
+
+        vector<vector<bool>>t(n , vector<bool>(n , false));
+
+        for(int i = 0 ; i < n ; i++) t[i][i] = true;
+
+        for(int L = 2 ; L <= n ;  L++){
+            for(int i = 0 ; i + L - 1 < n ; i++){
+                int j = i + L - 1;
+
+                if(L == 2 && s[i] == s[j]) t[i][j] = true;
+
+                else{
+                    if(s[i] == s[j] && t[i+1][j-1]) t[i][j] = true;
+                }
+            }
+        }
+
         vector<vector<string>>ans;
         vector<string>curr;
-        recurse(s , s.length() , 0 , curr , ans);
+
+        solve(0 , s , curr , ans , t);
+
         return ans;
     }
 };
