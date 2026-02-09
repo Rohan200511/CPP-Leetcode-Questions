37e62e65f7ea74267c140a859a3f3c0d55662b47
Rohan <rohangupta112005@gmail.com>
2026-02-09 22:40:30 +0530

    Time: 3 ms (9.29%) | Memory: 9.4 MB (28.59%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,22 +1,23 @@
 class Solution {
-public: 
-    vector<vector<int>>t;
-
-    int dfs(int m , int n , int i , int j){
-
-        if(i == m-1 && j == n-1) return 1;
-        if(i >= m || j >= n) return 0;
-
-        if(t[i][j] != -1) return t[i][j];
-
-        int down =  dfs(m , n , i+1 , j);
-        int right =  dfs(m , n , i , j+1);
-
-        return t[i][j] = down + right;
-    }
-
+public:
     int uniquePaths(int m, int n) {
-        t.assign(m , vector<int>(n , -1));
-        return dfs(m , n , 0 , 0);
+        if(m == 1 && n == 1) return 1; 
+        
+        vector<vector<int>>t(m , vector<int>(n , 0));
+
+        for(int i = 1 ; i < n ; i++){
+            t[0][i] = 1;
+        }
+
+        for(int j = 1 ; j < m ; j++){
+            t[j][0] = 1;
+        }
+
+        for(int i = 1 ; i < m ; i++){
+            for(int j = 1 ; j < n ; j++){
+                t[i][j] = t[i-1][j] + t[i][j-1];
+            }
+        }
+        return t[m-1][n-1];
     }
 };
