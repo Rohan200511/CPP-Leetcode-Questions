0ecd2199deadfe09dda6837d978aa11ff8b299a8
Rohan <rohangupta112005@gmail.com>
2026-02-14 16:14:42 +0530

    Time: 7 ms (44.31%) | Memory: 8.6 MB (92.98%) - LeetSync

--- 815-champagne-tower/champagne-tower.cpp
+++ 815-champagne-tower/champagne-tower.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
-    double t[101][101];
-    double solve(int poured , int i , int j){
-        if(i < 0 || j < 0 || i < j) return 0.0;
-
-        if(t[i][j] != -1.0) return t[i][j]; 
-
-        if(i == 0 && j == 0) return t[i][j] = poured;
-
-        double left_up = max(0.0 , (solve(poured , i - 1 , j - 1) - 1.0) / 2.0);
-        double right_up = max(0.0 , (solve(poured , i - 1 , j) - 1.0) / 2.0);
-
-
-        return t[i][j] =  left_up + right_up;
-    }
-
     double champagneTower(int poured, int query_row, int query_glass) {
-        for(int i = 0 ; i <= 100 ; i++){
-            for(int j = 0 ; j <= 100 ; j++){
-                t[i][j] = -1.0;
+        double t[101][101] = {0.0};
+
+        t[0][0] = poured;
+
+        for(int i = 1 ; i <= 100 ; i++){
+            for(int j = 0 ; j <= i ; j++){
+                double left_up = max(0.0 , (j - 1 >= 0) ? (t[i-1][j-1] - 1.0) / 2.0 : 0.0);
+                double right_up = max(0.0 , (j < i) ? (t[i-1][j] - 1.0) / 2.0 : 0.0);
+
+                t[i][j] = ((left_up + right_up) < 0) ? 0.0 : left_up + right_up;
             }
         }
-
-        return min(1.0 , solve(poured , query_row , query_glass));
+        return min(1.0, t[query_row][query_glass]);
     }
 };
