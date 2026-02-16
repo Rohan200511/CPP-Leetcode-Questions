ca54c0b8ecc16644cad76062f66fa63a04c1c148
Rohan <rohangupta112005@gmail.com>
2026-02-16 13:51:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Wave Array/wave-array.cpp
+++ Difficulty: Medium/Wave Array/wave-array.cpp
@@ -4,8 +4,9 @@ class Solution {
         // code here
         int n = arr.size();
         
-        for(int i = 0; i < n; i += 2){
-            if(i+1 < n && arr[i] < arr[i+1]) swap(arr[i] , arr[i+1]);
+        for(int i = 0 ; i < n - 1 ; i++){
+            swap(arr[i] , arr[i+1]);
+            i++;
         }
     }
 };
