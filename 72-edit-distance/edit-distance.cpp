8fcdb8d9f5f2767fe299e4a7a38e3387ff3b2d31
Rohan <rohangupta112005@gmail.com>
2026-02-19 22:57:03 +0530

    Time: 3 ms (91.49%) | Memory: 14.5 MB (15.36%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
-    int t[501][501];
-    int solve(string s1 , string s2 , int n , int m , int i , int j){
-        if(i == n) return m-j;
-        else if(j == m) return n-i;
+
+    int n , m;
+
+    vector<vector<int>>t;
+
+    int solve(const string& s1 , const string& s2 , int i , int j){
+        if(i == n) return m - j;
+        else if(j == m) return n - i;
+
         if(t[i][j] != -1) return t[i][j];
-        if(s1[i] == s2[j]) return t[i][j] = solve(s1,s2,n,m,i+1,j+1);
-        int insert = 1 + solve(s1,s2,n,m,i,j+1);
-        int del = 1 + solve(s1,s2,n,m,i+1,j);
-        int replace = 1 + solve(s1,s2,n,m,i+1,j+1);
+
+        if(s1[i] == s2[j]) return t[i][j] = solve(s1 , s2 , i + 1 , j + 1);
+
+        int insert = 1 + solve(s1 , s2 , i , j + 1);
+        int del =  1 + solve(s1 , s2 , i + 1 , j);
+        int replace = 1 + solve(s1 , s2 , i + 1 , j + 1);
 
         return t[i][j] = min({insert , del , replace});
     }
 
     int minDistance(string word1, string word2) {
-        int n = word1.length();
-        int m = word2.length();
-        memset(t,-1,sizeof(t));
-        return solve(word1,word2,n,m,0,0);
+        n = word1.length();
+        m = word2.length();
+
+        t.assign(n + 1 , vector<int>(m + 1 , -1));
+
+        return solve(word1 , word2 , 0 , 0);
     }
 };
