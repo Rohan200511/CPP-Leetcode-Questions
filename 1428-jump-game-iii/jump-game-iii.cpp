2d83d91c3888686a87c87bc15c29db809bdfb069
Rohan <rohangupta112005@gmail.com>
2026-05-17 14:13:15 +0530

    Time: 3 ms (45.57%) | Memory: 39.6 MB (35.05%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.cpp
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
+    int n;
+    vector<bool>vis;
+
+    bool dfs(vector<int>& arr , int ind){
+        if(ind > n || ind < 0 || vis[ind]) return false;
+        if(arr[ind] == 0) return true;
+
+        vis[ind] = true;
+
+        return dfs(arr , ind + arr[ind]) || dfs(arr , ind - arr[ind]);
+    }
+
     bool canReach(vector<int>& arr, int start) {
-        int n = arr.size();
-
-        queue<int>q;
-        vector<bool>vis(n , false);
-
-        q.push(start);
-        vis[start] = true;
-
-        while(!q.empty()){
-            int ind = q.front();
-            q.pop();
-
-            if(arr[ind] == 0) return true;
-
-            int frwd = ind + arr[ind];
-            int bkwd = ind - arr[ind];
-
-            if(frwd < n && !vis[frwd]){
-                q.push(frwd);
-                vis[frwd] = true;
-            }
-
-            if(bkwd >= 0 && !vis[bkwd]){
-                q.push(bkwd);
-                vis[bkwd] = true;
-            }
-        }
-        return false;
+        n = arr.size();
+        vis.resize(n , false);
+        return dfs(arr , start);
     }
 };
