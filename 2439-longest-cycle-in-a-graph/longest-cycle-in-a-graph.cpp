5ce455d7815debf8684689e5ca652d75ff4e8a8d
Rohan <rohangupta112005@gmail.com>
2026-07-12 21:46:48 +0530

    Time: 46 ms (48.86%) | Memory: 108.9 MB (60.44%) - LeetSync

--- 2439-longest-cycle-in-a-graph/longest-cycle-in-a-graph.cpp
+++ 2439-longest-cycle-in-a-graph/longest-cycle-in-a-graph.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
+    int ans = -1;
 
-    int result = -1;
-
-    void dfs(int u , const vector<int>& edges , vector<bool>& vis , vector<bool>& isRec , vector<int>& count){
-        vis[u] = true;
-        isRec[u] = true;
+    void dfs(int u , vector<bool>& vis , vector<bool>& recPath , vector<int>& edges , vector<int>& count){
+        vis[u] = recPath[u] = true;
 
         int v = edges[u];
 
         if(v != -1 && !vis[v]){
             count[v] = count[u] + 1;
-            dfs(v , edges , vis , isRec , count);
+            dfs(v , vis , recPath , edges , count);
         }
-        else if(v != -1 && isRec[v]){
-            result = max(result , count[u] - count[v] + 1);
+        else if(v != -1 && recPath[v]){
+            int temp = count[u] - count[v] + 1;
+
+            ans = max(ans , temp);
         }
-        isRec[u] = false;
+        recPath[u] = false;
     }
 
     int longestCycle(vector<int>& edges) {
         int n = edges.size();
+
         vector<bool>vis(n , false);
-        vector<bool>isRec(n , false);
-        vector<int>count(n , 1);
+        vector<bool>recPath(n , false);
+        vector<int>count(n , 0);
 
         for(int i = 0 ; i < n ; i++){
-            if(!vis[i]) dfs(i , edges , vis , isRec , count);
+            if(!vis[i]) dfs(i , vis , recPath , edges , count);
         }
-
-        return result;
+        return ans;
     }
 };
