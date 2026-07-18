809375f7f73568619dd8a73c884eef5d1ab7befb
Rohan <rohangupta112005@gmail.com>
2026-07-18 19:25:18 +0530

    Time: 1 ms (25.29%) | Memory: 8.7 MB (46.74%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
-    int t[101];
-    int solve(int i , string &s , int n){
-        if(t[i] != -1) return t[i];
-        if(i == n) return t[i] = 1;
-        if(s[i] == '0') return t[i] = 0;
 
-        int result = solve(i+1,s,n);
-        if(i+1 < n){
-            if(s[i] == '1' || (s[i] == '2' && s[i+1]<='6')){
-                result += solve(i+2,s,n);
+    int n;
+    vector<int>dp;
+
+    int solve(string& s , int i){
+        if(i == n) return 1;
+        if(s[i] == '0') return dp[i] = 0;
+
+        if(dp[i] != -1) return dp[i];
+
+        int ans = solve(s , i + 1);
+
+        if(i + 1 < n){
+            if(s[i] == '1' || s[i] == '2' && s[i + 1] <= '6'){
+                ans += solve(s , i + 2);
             }
         }
-        return t[i] = result;
+        return dp[i] = ans;
     }
 
     int numDecodings(string s) {
-        int n = s.length();
-        memset(t,-1,sizeof(t));
-        return solve(0,s,n);
+        n = s.length();
+        
+        dp.assign(n , -1);
+        return solve(s , 0);
     }
 };
