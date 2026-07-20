62a722db57d1f2bbee28eb12b8e58bbce492c3e3
Rohan <rohangupta112005@gmail.com>
2026-07-20 16:51:55 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (16.93%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
+
     int n , m;
-    vector<vector<int>>t;
+    vector<vector<int>>dp;
 
-    int dfs(vector<vector<int>>& grid , int i , int j){
-        if(i == n-1 && j == m-1) return grid[i][j];
-
+    int solve(vector<vector<int>>& grid , int i , int j){
         if(i >= n || j >= m) return INT_MAX;
+        if(i == n - 1 && j == m - 1) return grid[i][j];
 
-        if(t[i][j] != -1) return t[i][j];
+        if(dp[i][j] != -1) return dp[i][j];
 
-        int right = dfs(grid , i , j+1);
-        int down = dfs(grid , i+1 , j);
+        int right = solve(grid , i , j + 1);
+        int down = solve(grid , i + 1 , j);
 
-        return t[i][j] =  grid[i][j] + min(down , right);
+        return dp[i][j] = grid[i][j] + min(right , down);
     }
 
     int minPathSum(vector<vector<int>>& grid) {
         n = grid.size();
         m = grid[0].size();
-        t.assign(n , vector<int>(m , -1));
-        return dfs(grid, 0 , 0);
+
+        dp.assign(n , vector<int>(m , -1));
+
+        return solve(grid , 0 , 0);
     }
 };
