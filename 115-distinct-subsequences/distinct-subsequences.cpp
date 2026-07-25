d64ee10e6ce5fa2bff453b3d2806f50ba9068bde
Rohan <rohangupta112005@gmail.com>
2026-07-25 18:37:51 +0530

    Time: 27 ms (69.35%) | Memory: 44.3 MB (21.64%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
 
     int n , m;
-    vector<vector<int>>dp;
+    vector<vector<unsigned long long>>dp;
 
-    int solve(string& s , string& t , int i , int j){
+    /*int solve(string& s , string& t , int i , int j){
         if(j >= m) return 1;
         if(i >= n) return 0;
 
@@ -13,11 +13,26 @@ public:
         if(s[i] == t[j]) return dp[i][j] =  solve(s , t , i + 1 , j + 1) + solve(s , t , i + 1 , j);
         else return dp[i][j] = solve(s , t , i + 1 , j);
     }
-
+*/
     int numDistinct(string s, string t) {
         n = s.length();
         m = t.length();
-        dp.assign(n , vector<int>(m , -1));
-        return solve(s , t , 0 , 0);
+        // dp.assign(n , vector<int>(m , -1));
+        //return solve(s , t , 0 , 0);
+
+        dp.assign(n + 1 , vector<unsigned long long>(m + 1 , 0));
+
+        for (int i = 0; i <= n; i++)
+            dp[i][m] = 1;
+
+        for(int i = n - 1 ; i >= 0 ; i--){
+            for(int j = m - 1 ; j >= 0 ; j--){
+                if(s[i] == t[j]){
+                    dp[i][j] = dp[i + 1][j + 1] + dp[i + 1][j];
+                }
+                else dp[i][j] = dp[i + 1][j];
+            }
+        }
+        return dp[0][0];
     }
 };
