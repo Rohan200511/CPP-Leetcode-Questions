325d2b1709664900c33b44eb247c1669ac98309a
Rohan <rohangupta112005@gmail.com>
2026-08-01 15:24:28 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (99.74%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool solve(vector<int>& nums, int i, int j, int p1, int p2, bool turn) {
+        if (i > j)
+            return p1 >= p2;
+
+        if (turn) { 
+            return solve(nums, i + 1, j, p1 + nums[i], p2, false) ||
+                   solve(nums, i, j - 1, p1 + nums[j], p2, false);
+        } 
+        
+        else { 
+            return solve(nums, i + 1, j, p1, p2 + nums[i], true) &&
+                   solve(nums, i, j - 1, p1, p2 + nums[j], true);
+        }
+    }
+
+    bool predictTheWinner(vector<int>& nums) {
+        return solve(nums, 0, nums.size() - 1, 0, 0, true);
+    }
+};
