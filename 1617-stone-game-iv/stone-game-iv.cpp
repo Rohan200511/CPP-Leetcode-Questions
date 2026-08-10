06523200a1076b92c9410a5b90dae4b197e0f628
Rohan <rohangupta112005@gmail.com>
2026-08-10 12:03:27 +0530

    Time: 39 ms (67.82%) | Memory: 16.8 MB (42.05%) - LeetSync

---
+++ 1617-stone-game-iv/stone-game-iv.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+
+    vector<int>dp;
+
+    bool solve(int n){
+        if(n == 0) return false;
+        
+        if(dp[n] != -1) return dp[n];
+
+        bool take = false;
+
+        for(int i = 1 ; i * i <= n ; i++){
+            int sq = i * i;
+
+            take = solve(n - sq);
+
+            if(take == false) return dp[n] = true;
+        }
+        return dp[n] = false;
+    }
+
+    bool winnerSquareGame(int n) {
+        dp.assign(n + 1 , -1);
+        return solve(n);
+    }
+};
