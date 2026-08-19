1d93c0bcfa05fc7a6d052ef4e8250a71b32c3617
Rohan <rohangupta112005@gmail.com>
2026-08-19 23:06:23 +0530

    Time: 194 ms (9.70%) | Memory: 49 MB (12.62%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,46 +1,64 @@
 class Solution {
 public:
 
-    typedef pair<int , pair<int , int>> P;
-    vector<vector<int>>directions = {{1 , 0} , {0 , 1} , {-1 , 0} , {0 , -1}};
+    vector<vector<int>>dirs = {{1 , 0} , {0 , 1} , {-1 , 0} , {0 , -1}};
+
+    bool canReach(vector<vector<int>>& heights , int n , int m , int limit){
+
+        queue<pair<int , int>>q;
+        vector<vector<bool>>vis(n , vector<bool>(m , false));
+        q.push({0 , 0});
+        vis[0][0] = true;
+
+        while(!q.empty()){
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
+
+            if(r == n - 1 && c == m - 1) return true;
+
+            for(auto& dir : dirs){
+                int nr = r + dir[0];
+                int nc = c + dir[1];
+
+                if(nr < 0 || nc < 0 || nr >= n || nc >= m) continue;
+                
+                int diff = abs(heights[r][c] - heights[nr][nc]);
+
+                if(!vis[nr][nc] && diff <= limit){
+                    vis[nr][nc] = true;
+                    q.push({nr , nc});
+                }
+            }
+
+        }
+        return false;
+    }
+
     int minimumEffortPath(vector<vector<int>>& heights) {
         int n = heights.size();
         int m = heights[0].size();
-        vector<vector<int>>result(n , vector<int>(m , INT_MAX));
 
-        priority_queue<P , vector<P> , greater<P>>pq;
+        int left = 0;
+        int right = INT_MIN;
 
-        auto isSafe = [&](int x , int y){
-            return x >= 0 && x < n && y >= 0 && y < m;
-        };
-
-        result[0][0] = 0;
-        pq.push({0 , {0 , 0}});
-
-        while(pq.size()){
-            int diff = pq.top().first;
-            auto coord = pq.top().second;
-            pq.pop();
-            int x = coord.first;
-            int y = coord.second;
-
-            if(x == n-1 && y == m-1) return diff;
-
-            for(auto& dir : directions){
-                int nx = x + dir[0];
-                int ny = y + dir[1];
-
-                if(isSafe(nx , ny)){
-                    int absDiff = abs(heights[x][y] - heights[nx][ny]);
-                    int maxDiff = max(diff , absDiff);
-
-                    if(result[nx][ny] > maxDiff){
-                        result[nx][ny] = maxDiff;
-                        pq.push({maxDiff , {nx , ny}});
-                    }
-                }
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                right = max(right , heights[i][j]);
             }
         }
-        return result[n-1][m-1];
+
+        int ans = right;
+
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+
+            if(canReach(heights , n , m , mid)){
+                ans = mid;
+                right = mid - 1;
+            }
+            else left = mid + 1;
+        }
+        return ans;
     }
 };
