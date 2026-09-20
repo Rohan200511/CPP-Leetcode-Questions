e5e6a197a0c5393a6bac9e572d30d423c8149311
Rohan <rohangupta112005@gmail.com>
2026-09-20 19:10:59 +0530

    Time: 1 ms (44.61%) | Memory: 16.5 MB (12.59%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -2,33 +2,31 @@ class Solution {
 public:
 
     int n;
-
     vector<vector<int>>dp;
 
     int solve(vector<int>& prices , int i , bool buy){
         if(i >= n) return 0;
 
-        if(dp[i][buy] != -1) return dp[i][buy];
-
-        int profit = 0;
+        if(dp[i][buy] != -1e9) return dp[i][buy];
 
         if(buy){
-            int take = -prices[i] + solve(prices , i + 1 , !buy);
-            int notTake = solve(prices , i + 1 , buy);
-            profit = max({profit , take , notTake});
+            return dp[i][buy] = max(
+                solve(prices , i + 1 , buy),
+                -prices[i] + solve(prices , i + 1 , !buy)
+            );
+        }
+        else{
+            return dp[i][buy] = max(
+                solve(prices , i + 1 , buy),
+                prices[i] + solve(prices , i + 2 , !buy)
+            );
         }
 
-        else{
-            int sell = prices[i] + solve(prices , i + 2 , !buy);
-            int not_sell = solve(prices , i + 1 , buy);
-            profit = max({sell , not_sell , profit});
-        }
-        return dp[i][buy] = profit;
     }
 
     int maxProfit(vector<int>& prices) {
         n = prices.size();
-        dp.assign(n , vector<int>(2 , -1));
+        dp.assign(n , vector<int>(2 , -1e9));
         return solve(prices , 0 , true);
     }
 };
