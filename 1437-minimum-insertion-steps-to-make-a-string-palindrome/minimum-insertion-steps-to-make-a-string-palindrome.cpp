960eef152315e8fe14fcc971ea454510b051a6d0
Rohan <rohangupta112005@gmail.com>
2026-09-22 18:57:15 +0530

    Time: 27 ms (71.44%) | Memory: 31.5 MB (38.58%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-
-    int n;
-    vector<vector<int>>dp;
-
-    int solve(const string& s , int i , int j){
-        if(i >= j) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(s[i] == s[j]) return dp[i][j] = solve(s , i + 1 , j - 1);
-        else return dp[i][j] = 1 + min(solve(s , i + 1 , j) , solve(s , i , j - 1));
-    }
-
     int minInsertions(string s) {
-        n = s.length();
-        dp.assign(n + 1 , vector<int>(n + 1 , -1));
-        return solve(s , 0 , n - 1);
+        int n = s.length();
+        vector<vector<int>>dp(n + 1 , vector<int>(n + 1 , 0));
+        
+        for(int len = 1 ; len <= n ; len++){
+            for(int i = 0 ; i + len - 1 < n ; i++){
+                int j = i + len - 1;
+
+                if(len == 1) dp[i][i] = 0;
+
+                else if(s[i] == s[j]) dp[i][j] = dp[i + 1][j - 1];
+
+                else dp[i][j] = 1 + min(dp[i + 1][j] , dp[i][j - 1]);
+            }
+        }
+        return dp[0][n - 1];
     }
 };
