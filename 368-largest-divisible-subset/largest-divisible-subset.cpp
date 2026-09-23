f59070b8f1848f2d611ba2882291016b4d049a2c
Rohan <rohangupta112005@gmail.com>
2026-09-23 16:24:03 +0530

    Time: 8 ms (82.39%) | Memory: 12.5 MB (78.46%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -5,30 +5,29 @@ public:
         sort(nums.begin() , nums.end());
         vector<int>dp(n , 1);
         vector<int>prev(n , -1);
-        int maxL = 1;
-        int last_index = 0;
+        int maxLen = 1;
+        int maxIdx = 0;
 
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < i ; j++){
                 if(nums[i] % nums[j] == 0){
                     if(dp[i] < dp[j] + 1){
-                        dp[i] = dp[i] + 1;
+                        dp[i] = dp[j] + 1;
                         prev[i] = j;
                     }
                 }
-
-                if(maxL < dp[i]){
-                    maxL = dp[i];
-                    last_index = i;
-                }
+            }
+            if(dp[i] > maxLen){
+                maxLen = dp[i];
+                maxIdx = i;
             }
         }
 
         vector<int>ans;
-
-        while(last_index != -1){
-            ans.push_back(nums[last_index]);
-            last_index = prev[last_index];
+        
+        while(maxIdx >= 0){
+            ans.push_back(nums[maxIdx]);
+            maxIdx = prev[maxIdx];
         }
 
         return ans;
