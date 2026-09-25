33a17a21f2275fa2524cb4e6dffa6476af747739
Rohan <rohangupta112005@gmail.com>
2026-09-25 11:55:14 +0530

    Time: 2 ms (58.29%) | Memory: 9.7 MB (10.91%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.cpp
+++ 10-regular-expression-matching/regular-expression-matching.cpp
@@ -1,34 +1,31 @@
 class Solution {
-public:
+public: 
 
     int n , m;
 
     vector<vector<int>>dp;
 
-    bool solve(string& s , string& p , int i , int j){
-        if (j == m)
-            return i == n;
+    bool solve(const string& s , const string& p , int i , int j){
+        if(j == m) return i == n;
 
         if(dp[i][j] != -1) return dp[i][j];
 
-        bool firstMatch = (i < n) && ((s[i] == p[j]) || p[j] == '.');
+        bool match = (i < n) && ((s[i] == p[j]) || p[j] == '.');
 
-        if(j + 1 < m && p[j + 1] == '*'){
+        if(j + 1 < m && p[j + 1 ] == '*'){
+            bool take = match && solve(s , p , i + 1 , j);
+
             bool skip = solve(s , p , i , j + 2);
-            bool consume = firstMatch && solve(s , p , i + 1 , j);
 
-            return dp[i][j] = skip || consume;
-        } 
-
-        return dp[i][j] = firstMatch && solve(s , p , i + 1 , j + 1);
-    }    
+            return dp[i][j] = skip || take;
+        }
+        return dp[i][j] = match && solve(s , p , i + 1 , j + 1);
+    }
 
     bool isMatch(string s, string p) {
         n = s.length();
         m = p.length();
-
         dp.assign(n + 1 , vector<int>(m + 1 , -1));
-
         return solve(s , p , 0 , 0);
     }
 };
